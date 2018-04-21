fde679eecdfebc4ac051a483e9d7f6c8d5b8353a
Mogyorósi Bálint <mogyorosibalint@Mogyorosi-MacBook-Air.local>
2018-04-21 19:16:27 +0200

    register routing and rendering

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
 
 import config
 import data_manager
@@ -8,8 +8,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def route_home():
     return render_template('home.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def route_register():
+    if request.method == 'GET':
+        return render_template('register.html')
 
 
 if __name__ == "__main__":
