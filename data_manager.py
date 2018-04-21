dba1b5155f6116d33c3c9cd67765e605a2d16e21
Mogyorósi Bálint <mogyorosibalint@Mogyorosi-MacBook-Air.local>
2018-04-21 20:38:31 +0200

    added get user by name, and also fixed the query string

--- data_manager.py
+++ data_manager.py
@@ -4,7 +4,16 @@ import connection
 @connection.connection_handler
 def get_single_user(cursor, user_id):
     cursor.execute("""
-        SELECT * FROM user
+        SELECT * FROM "user"
         WHERE id = %(id)s;
-    """, {'id': id})
+    """, {'id': user_id})
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_single_user_by_name(cursor, username):
+    cursor.execute("""
+        SELECT * FROM "user"
+        WHERE username = %(username)s;
+    """, {'username': username})
     return cursor.fetchall()
