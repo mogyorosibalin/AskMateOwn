2f91feddabc2cc6f5e1e1fdd395f506dd738b8f1
Mogyorósi Bálint <mogyorosibalint@Mogyorosi-MacBook-Air.local>
2018-04-21 20:15:58 +0200

    get_registration_error_message added to validate the user_registration

--- util.py
+++ util.py
@@ -1,5 +1,23 @@
+import data_manager
+
+
 def get_dict_from_request(form_data):
     output = dict()
     for field in form_data:
         output[field] = form_data[field].strip()
     return output
+
+
+def get_registration_error_messages(user):
+    error_messages = list()
+    if len(user["username"]) < 4:
+        error_messages.append('The Username must contain at least 4 characters!')
+    if len(data_manager.get_single_user_by_name(user["username"])):
+        error_messages.append('This username is already in use! Please choose another one!')
+    if len(user["username"]) > 20:
+        error_messages.append('The Username mustn\'t contain more than 20 characters!')
+    if len(user["password"]) < 8:
+        error_messages.append('The Password must contain at least 8 characters!')
+    if user["password"] != user["password_again"]:
+        error_messages.append('The two Password must be the same!')
+    return error_messages
