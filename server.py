e510b550f54ea400ebfd550e84157ecaaf1a636f
Mogyorósi Bálint <mogyorosibalint@Mogyorosi-MacBook-Air.local>
2018-04-22 21:33:10 +0200

    added routing for login and list-users

--- server.py
+++ server.py
@@ -33,5 +33,31 @@ def route_register():
             return redirect('/register')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def route_login():
+    if request.method == 'GET':
+        user = util.get_data_from_session('user')
+        if user is None:
+            user = {"username": ""}
+        error_messages = util.get_data_from_session('error_messages')
+        return render_template('login.html', user=user, error_messages=error_messages)
+    elif request.method == 'POST':
+        user = util.get_dict_from_request(request.form)
+        error_messages = util.get_login_error_messages(user)
+        if len(error_messages) == 0:
+            session['username'] = user["username"]
+            return redirect('/')
+        else:
+            session["user"] = user
+            session["error_messages"] = error_messages
+            return redirect('/login')
+
+
+@app.route('/list-users')
+def route_list_users():
+    users = data_manager.get_all_user()
+    return render_template('list-users.html', users=users)
+
+
 if __name__ == "__main__":
     app.run()
