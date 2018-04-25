40e8eb325f928793a353251311798fb3d62efe30
Mogyorósi Bálint <mogyorosibalint@Mogyorosi-MacBook-Air.local>
2018-04-25 15:13:46 +0200

    textarea added to focus selector

--- static/js/main.js
+++ static/js/main.js
@@ -14,7 +14,7 @@ $('.page-header .overlay').on('click', function() {
     $(this).removeClass('active');
 });
 
-$('.form-group input').focusin(function() {
+$('.form-group input, .form-group textarea').focusin(function() {
    $(this).parent().addClass('active');
 }).focusout(function() {
     $(this).parent().removeClass('active');
