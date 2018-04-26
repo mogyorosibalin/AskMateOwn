cb765b6d4c3735a3e70d2d6b4c2db9a2b970c441
Mogyorósi Bálint <mogyorosibalint@Mogyorosi-MacBook-Air.local>
2018-04-26 15:14:14 +0200

    404 not found page error handling added, basic html

--- server.py
+++ server.py
@@ -220,5 +220,10 @@ def route_delete_answer(answer_id=None):
     return redirect('/list')
 
 
+@app.errorhandler(404)
+def route_page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run()
